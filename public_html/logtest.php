ec51f69accb9eaacf5efd7b8b5644163e73c0333
InternMo <mohammed@haveabyte.nl>
2021-09-22 09:21:17 +0200

    Updated to prevent injections

--- public_html/logtest.php
+++ public_html/logtest.php
@@ -1,23 +1,60 @@
 <?php
 
-class Database {
-
-    var $tbname = 'mo_test_db';
 
 
-    public function connect() {
-        $connection = new mysqli('localhost', 'mohammed', '1234', 'wp_zorgdossier');
-        return $connection;
+class Log {
+
+    var $table = '';
+    private $wpdb;
+
+    public function __construct(&$wpdb) {
+        $this->table = 'mo_test_db';
+        $this->wpdb = $wpdb;
     }
 
-    public function saveRecords($user_id, $activity, $date) {
+    public function log($action, $type, $user_id = 0, $meta = '') {
 
-        $conn=$this->connect();
-        mysqli_query($conn,"INSERT INTO ".$this->tbname."(user_id,activity,date) VALUES('$user_id', '$activity', '$date')") or die(mysqli_error($conn));
-        echo "<div style='padding:20px; background-color:yellow;'>
-        Data added successfully</div>";
+        if ($action == '' || $type == '')
+           throw new Exception('Error in aanroep');
+
+        if ($user_id == 0) { //User ID meegegeven? Zo niet bekijken of we die kunnen achterhalen
+         
+            
 
         }
+
+        $fields = [
+            'user_id' => $user_id,
+            'type' => $type,
+            'insertdate' => time()
+        ];
+
+        return $this->save($fields);
+
     }
 
+    private function save($fields = []) {
+
+        //mysqli_query($this->conn,"INSERT INTO `".$this->table."` (user_id,activity,insertdate) VALUES('$user_id', '$activity', $date)") or die(mysqli_error($conn));
+
+        $fields = [
+            'user_id' => $user_id,
+            'type' => $type,
+            'insertdate' => time()
+        ];
+
+        $columns = implode(", ",array_keys($fields));
+        $escaped_values = array_map('mysqli_real_escape_string', array_values($fields));
+        $values = implode(", ", $escaped_values);
+
+        $wpdb->query( $wpdb->prepare("INSERT INTO `.$this->table.` ($columns) VALUES($values)"));
+        echo "Data added"
+
+        //$wpdb->query( $wpdb->prepare('INSERT INTO xxx (a,b,c) value(%s, %s, %d)', ['a','b',1]));
+
+
+
+        
+    }
+}
 ?>
--- public_html/logtestadd.php
+++ public_html/logtestadd.php
@@ -10,10 +10,15 @@ if (isset($_POST['submit_log'])) {
     include "logtest.php";
     $user_id = $_POST['user_id'];
     $activity = $_POST['activity'];
-    $date = date('Y-m-d G:i:s');
+    $date = time();
 
-    $obj = new Database();
-    $obj->saveRecords($user_id,$activity,$date);
+    $connection = new mysqli('localhost', 'mohammed', '1234', 'wp_zorgdossier');
+
+    $obj = new Log($connection);
+    if ($obj->log($action, $type))
+        echo "<div style='padding:20px; background-color:yellow;'>
+        Data added successfully</div>";
+
 
 }
 
